c1690abad0a05ba7407d28c78d8143cc766a6393
Naguirrel <agu24479@uvg.edu.gt>
2025-09-11 21:29:19 -0600

    Inicio Lab 8

---
+++ app/src/main/java/com/naguirrel/plataslabs8_muchos/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.naguirrel.plataslabs8_muchos
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.naguirrel.plataslabs8_muchos.ui.theme.PlatasLabs8_muchosTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PlatasLabs8_muchosTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PlatasLabs8_muchosTheme {
+        Greeting("Android")
+    }
+}
